75fb6441375e789b024092910a7215ce3a2d4a8d
James Maynard and Matthew Terrill <jameskirkmaynard@gmail.com>
2015-03-19 15:15:47 -0700

    Add edit functionality.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,7 @@ class RecipesController < ApplicationController
     @recipe = @user.recipes.new( recipe_params )
     if @recipe.save
       flash[:notice] = "Recipe successfully added"
-      redirect_to user_path(@recipe.user)
+      redirect_to user_recipe_path(@user, @recipe)
     else
       render :new
     end
@@ -18,6 +18,21 @@ class RecipesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to user_recipe_path(@user, @recipe)
+    else
+      render :edit
+    end
   end
 
   def destroy
