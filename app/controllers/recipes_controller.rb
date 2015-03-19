7fd2063c274b10eee8ed96b4e261522db73207a3
James Maynard and Matthew Terrill <jameskirkmaynard@gmail.com>
2015-03-19 10:30:58 -0700

    Add functionality for user to add recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,22 @@
 class RecipesController < ApplicationController
   def new
-    @user = User.find(params[(:user_id)])
+    @user = User.find(params[:user_id])
+    @recipe = @user.recipes.new
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @recipe = @user.recipes.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = "Recipe successfully added"
+      redirect_to user_path(@recipe.user)
+    else
+      render :new
+    end
+  end
 
+private
+  def recipe_params
+    params.require(:recipe).permit(:name, :instructions, :ingredients)
   end
 end
